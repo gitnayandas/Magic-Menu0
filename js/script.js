479e444f21d06ae9c7cb8e65bcf310000e2775e3
gitnayandas <nayanchandradas@hotmail.com>
2024-06-16 11:35:01 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// JavaScript to handle active link and dark mode toggle
+
+// Get all list items
+
+
+// Function to activate the clicked link
+
+
+// Add event listener to each list item
+
+
+// Get dark mode toggle and navigation elements
+
+
+// Add event listener to dark mode toggle
+
+
+// Restore dark mode from local storage
+
